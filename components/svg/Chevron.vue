86a864749d90c9e6c6d550b855d8701213d21e18
Conner P <conner.c.wells@gmail.com>
2023-02-09 10:20:25 -0800

    adds Chevron svg

---
+++ components/svg/Chevron.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    width="17"
+    height="9"
+    viewBox="0 0 17 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M15.4151 7.875L8.59624 1.58936L1.77734 7.875"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
